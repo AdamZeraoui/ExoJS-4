25680ff409008cfa92a55dc2cafcf0986c602cd0
AdamZeraoui <adam.zeraoui@hotmail.fr>
2024-03-04 16:54:27 +0100

    Update shadow-box

--- effect.js
+++ effect.js
@@ -1,63 +1,48 @@
-const icones = document.querySelectorAll(".icone")
-const squares = document.querySelectorAll(".square")
-const texts = document.querySelectorAll(".text")
 
 
-icones.forEach(function(icone){
-        icone.addEventListener("click",function(){
-        if(icone.classList == "icone"){
-            icone.style.transform = 'rotate(360deg)';
-            icone.classList.toggle("icone") 
-            icone.classList.toggle("iconeBis")
-            
-        }else{
-            icone.style.transform = 'rotate(0deg)';
-            icone.classList.toggle("icone")
-            icone.classList.toggle("iconeBis")
-            
-        }
-
-    })
-   
-});
-
-
-
-squares.forEach(function(square){
-    square.addEventListener("click",function(){
-        if(square.classList == "square"){
-            square.classList.toggle("square") 
-            square.classList.toggle("squareBis")
-        
-        }else{
-            square.classList.toggle("square")
-            square.classList.toggle("squareBis")
-        
-        }
-
-    })
-   
-});
-
-
-texts.forEach(function(text){
-    text.addEventListener("click",function(){
-        if(text.classList == "text"){
-            text.classList.toggle("text")
-            text.classList.toggle("textBis")
-
-        }else{
-            text.classList.toggle("text")
-            text.classList.toggle("textBis")
-
-        }
-    })
-
-});
-
-const click = document.body
-click.onclick = function(){
- document.documentElement.style.backgroundColor = "red"
-click.style.backgroundColor =  "rgba(160, 160, 160, 0)"
-
+function toggleClass(element, classRemoved, classAdded) {
+    if (element.classList.contains(classRemoved)) {
+        element.classList.toggle(classRemoved);
+        element.classList.toggle(classAdded);
+    } else {
+        element.classList.toggle(classAdded);
+        element.classList.toggle(classRemoved);
+    }
 }
+
+const icones = document.querySelectorAll(".icone");
+const squares = document.querySelectorAll(".square");
+const texts = document.querySelectorAll(".text");
+const initialBackgroundColor = window.getComputedStyle(document.body).getPropertyValue("background-color");
+
+squares.forEach(function(square) {
+    square.addEventListener("click", function() {
+        
+        const backgroundColor = window.getComputedStyle(square).getPropertyValue("background-color");
+
+        if (document.body.style.backgroundColor == backgroundColor) {
+            document.body.style.backgroundColor = initialBackgroundColor;
+            square.style.boxShadow = ""
+        } else {
+            document.body.style.backgroundColor = backgroundColor;
+            square.style.boxShadow = `0px 50px 20px -40px ${backgroundColor}`;
+        }
+
+
+
+        icones.forEach(function(icone) {
+            if (icone.parentElement == square) {
+                toggleClass(icone, "icone", "iconeBis");
+                icone.style.transform = (icone.classList.contains("icone")) ? 'rotate(0deg)' : 'rotate(360deg)';
+            }
+        });
+        
+        toggleClass(square, "square", "squareBis");
+        
+        texts.forEach(function(text) {
+            if (text.parentElement == square) {
+                toggleClass(text, "text", "textBis");
+            }
+        });
+    });
+});
